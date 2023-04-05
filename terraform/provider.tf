6e765a86307f0eaa05bcb37f860589e8d9d9c0a3
root <root@ip-172-31-3-190.ap-south-1.compute.internal>
2023-04-05 05:34:10 +0000

    infra code is ready

---
+++ terraform/provider.tf
@@ -0,0 +1,5 @@
+provider "aws" {
+region = "ap-south-1"
+access_key = "AKIAXXLCWT75RFQGCGPG"
+secret_key = "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"
+}
---
+++ terraform/variable.tf
@@ -0,0 +1,15 @@
+variable "ami_id" {
+default = "ami-0376ec8eacdf70aae"
+}
+
+variable "instance_type" {
+default = "t2.micro"
+}
+
+variable "key_name" {
+default = "project"
+}
+
+variable "name" {
+default = "my-instance"
+}
